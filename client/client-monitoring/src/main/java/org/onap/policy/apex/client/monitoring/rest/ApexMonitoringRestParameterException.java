adc709cdb604109b4fc7990c96224bd303f139ca
ramverma <ram.krishna.verma@ericsson.com>
2018-07-06 16:29:16 +0100

    Adding client monitoring module to apex-pdp
    Change-Id: I6b4d00a5e49326f62f3585526ede8e89df99d7e5
    Issue-ID: POLICY-864
    Signed-off-by: ramverma <ram.krishna.verma@ericsson.com>

---
+++ client/client-monitoring/src/main/java/org/onap/policy/apex/client/monitoring/rest/ApexMonitoringRestParameterException.java
@@ -0,0 +1,49 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2016-2018 Ericsson. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.apex.client.monitoring.rest;
+
+/**
+ * A run time exception used to report parsing and parameter input errors
+ *
+ * User: ewatkmi Date: 31 Jul 2017
+ */
+public class ApexMonitoringRestParameterException extends IllegalArgumentException {
+    private static final long serialVersionUID = 6520231162404452427L;
+
+    /**
+     * Create an ApexServicesRestParameterException with a message.
+     *
+     * @param message the message
+     */
+    public ApexMonitoringRestParameterException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Create an ApexServicesRestParameterException with a message and an exception.
+     *
+     * @param message the message
+     * @param throwable The exception that caused the exception
+     */
+    public ApexMonitoringRestParameterException(final String message, final Throwable throwable) {
+        super(message, throwable);
+    }
+}
