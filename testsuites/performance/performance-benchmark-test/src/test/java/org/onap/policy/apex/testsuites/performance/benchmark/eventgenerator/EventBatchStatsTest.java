b001bf42d42978c192f7c3550c9626a9bf341144
liamfallon <liam.fallon@ericsson.com>
2018-11-20 15:59:50 +0000

    Refactor Apex benchmark tests
    Apex performance tests are refactored and reorganised
    to be all in the one place and to be coherent.
    
    A test REST server that can generate events in batches
    is introduced and this can be configured to respond to REST
    calls from Apex with various batch sizes that can be varied
    for performance tests.
    
    Actual performance tests are added for the various executors
    in Apex.
    
    Change-Id: I692b41e1e4451e821a618c64dead5cc8e9685d08
    Issue-ID: POLICY-812
    Signed-off-by: liamfallon <liam.fallon@ericsson.com>

---
+++ testsuites/performance/performance-benchmark-test/src/test/java/org/onap/policy/apex/testsuites/performance/benchmark/eventgenerator/EventBatchStatsTest.java
@@ -0,0 +1,57 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2018 Ericsson. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.apex.testsuites.performance.benchmark.eventgenerator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * Test the EventBatchStats class.
+ *
+ */
+public class EventBatchStatsTest {
+
+    @Test
+    public void test() {
+        EventBatchStats stats = new EventBatchStats(new EventBatch(1, "Label"));
+        assertNotNull(stats);
+
+        assertTrue(stats.getBatchNumber() >= 0);
+        assertEquals(1, stats.getBatchSize());
+        assertEquals("Label", stats.getApexClient());
+
+        List<EventBatchStats> statsList = new ArrayList<>();
+        statsList.add(stats);
+
+        EventBatchStats totalStats = new EventBatchStats(statsList);
+        assertEquals(stats.getBatchSize(), totalStats.getBatchSize());
+
+        List<EventBatchStats> emptyStatsList = new ArrayList<>();
+        EventBatchStats emptyStats = new EventBatchStats(emptyStatsList);
+        assertEquals(0, emptyStats.getBatchSize());
+    }
+}
