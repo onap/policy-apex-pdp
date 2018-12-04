8ad058976d132e6e8288c8ebca8879329743a620
liamfallon <liam.fallon@est.tech>
2018-12-04 22:36:39 +0000

    Support HTTP headers in REST Requestor
    Change-Id: Iea4211e7d3324f5ea6244f45c91241a4067d8d33
    Issue-ID: POLICY-1222
    Signed-off-by: liamfallon <liam.fallon@est.tech>
    (cherry picked from commit c01eff1ee4aff0cc4b177a0bab5c6b96a9efb647)

---
+++ plugins/plugins-event/plugins-event-carrier/plugins-event-carrier-restrequestor/src/test/java/org/onap/policy/apex/plugins/event/carrier/restrequestor/ApexRestRequestTest.java
@@ -0,0 +1,49 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2018 Ericsson. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.apex.plugins.event.carrier.restrequestor;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Test the ApexRestRequest class.
+ */
+public class ApexRestRequestTest {
+
+    @Test
+    public void testApexRestRequest() {
+        final String eventName = "EventName";
+        final String eventString = "The Event String";
+
+        ApexRestRequest rr = new ApexRestRequest(1, eventName, eventString);
+
+        assertEquals(1, rr.getExecutionId());
+        assertEquals(eventName, rr.getEventName());
+        assertEquals(eventString, rr.getEvent());
+
+        rr.setTimestamp(1234567);
+        assertEquals(1234567, rr.getTimestamp());
+
+        assertEquals("ApexRestRequest [executionId=1, eventName=EventName, event=The Event String, timestamp=1234567]",
+                        rr.toString());
+    }
+}
