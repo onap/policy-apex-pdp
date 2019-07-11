445ca277e00718c4d8c6bf76f98783d6c080425c
ning.xi <ning.xi@est.tech>
2019-07-11 05:14:10 +0000

    Make URL configurable in REST Requestor/Client
    Change-Id: Ifb9870636996d1af786f05fcef7aa811d6f57532
    Issue-ID: POLICY-1743
    Signed-off-by: ning.xi <ning.xi@est.tech>

--- plugins/plugins-event/plugins-event-carrier/plugins-event-carrier-restrequestor/src/test/java/org/onap/policy/apex/plugins/event/carrier/restrequestor/ApexRestRequestTest.java
+++ plugins/plugins-event/plugins-event-carrier/plugins-event-carrier-restrequestor/src/test/java/org/onap/policy/apex/plugins/event/carrier/restrequestor/ApexRestRequestTest.java
@@ -1,19 +1,20 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2018 Ericsson. All rights reserved.
+ *  Modifications Copyright (C) 2019 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
@@ -23,6 +24,8 @@ package org.onap.policy.apex.plugins.event.carrier.restrequestor;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
+import java.util.Properties;
 
 /**
  * Test the ApexRestRequest class.
@@ -34,11 +37,14 @@ public class ApexRestRequestTest {
         final String eventName = "EventName";
         final String eventString = "The Event String";
 
-        ApexRestRequest rr = new ApexRestRequest(1, eventName, eventString);
+        Properties properties = new Properties();
+        properties.put("key", "value");
+        ApexRestRequest rr = new ApexRestRequest(1, properties, eventName, eventString);
 
         assertEquals(1, rr.getExecutionId());
         assertEquals(eventName, rr.getEventName());
         assertEquals(eventString, rr.getEvent());
+        assertEquals(properties, rr.getExecutionProperties());
 
         rr.setTimestamp(1234567);
         assertEquals(1234567, rr.getTimestamp());
