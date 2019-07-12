45662f273e7f0319fbe2e977ae980ace74f324e7
Henry.Sun <henry.a.sun@est.tech>
2019-07-12 08:42:13 +0000

    add status filter in restclient and restrequestor
    Issue-ID: POLICY-1742
    Change-Id: Ib4984b18c0353cddb12c13c352277728be691bf5
    Signed-off-by: Henry.Sun <henry.a.sun@est.tech>

--- plugins/plugins-event/plugins-event-carrier/plugins-event-carrier-restclient/src/main/java/org/onap/policy/apex/plugins/event/carrier/restclient/RestClientCarrierTechnologyParameters.java
+++ plugins/plugins-event/plugins-event-carrier/plugins-event-carrier-restclient/src/main/java/org/onap/policy/apex/plugins/event/carrier/restclient/RestClientCarrierTechnologyParameters.java
@@ -28,6 +28,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
 import org.onap.policy.apex.service.parameters.carriertechnology.CarrierTechnologyParameters;
 import org.onap.policy.common.parameters.GroupValidationResult;
 import org.onap.policy.common.parameters.ValidationStatus;
@@ -46,6 +50,8 @@ import org.onap.policy.common.utils.validation.ParameterValidationUtils;
  *
  * @author Joss Armstrong (joss.armstrong@ericsson.com)
  */
+@Setter
+@Getter
 public class RestClientCarrierTechnologyParameters extends CarrierTechnologyParameters {
     /** The supported HTTP methods. */
     public enum HttpMethod {
@@ -63,13 +69,17 @@ public class RestClientCarrierTechnologyParameters extends CarrierTechnologyPara
 
     // Commonly occurring strings
     private static final String HTTP_HEADERS = "httpHeaders";
+    private static final String HTTP_CODE_FILTER = "httpCodeFIlter";
 
     private String url = null;
     private HttpMethod httpMethod = null;
     private String[][] httpHeaders = null;
+
     private static final Pattern patternProperKey = Pattern.compile("(?<=\\{)[^}]*(?=\\})");
     private static final Pattern patternErrorKey = Pattern.compile(
         "(\\{[^\\{}]*.?\\{)|(\\{[^\\{}]*$)|(\\}[^\\{}]*.?\\})|(^[^\\{}]*.?\\})|\\{\\s*\\}");
+
+    private String httpCodeFilter = "[2][0-9][0-9]";
 
     /**
      * Constructor to create a REST carrier technology parameters instance and register the instance with the parameter
@@ -86,57 +96,12 @@ public class RestClientCarrierTechnologyParameters extends CarrierTechnologyPara
     }
 
     /**
-     * Gets the URL for the REST request.
-     *
-     * @return the URL
-     */
-    public String getUrl() {
-        return url;
-    }
-
-    /**
-     * Sets the URL for the REST request.
-     *
-     * @param incomingUrl the URL
-     */
-    public void setUrl(final String incomingUrl) {
-        this.url = incomingUrl;
-    }
-
-    /**
-     * Gets the HTTP method to use for the REST request.
-     *
-     * @return the HTTP method
-     */
-    public HttpMethod getHttpMethod() {
-        return httpMethod;
-    }
-
-    /**
-     * Sets the HTTP method to use for the REST request.
-     *
-     * @param httpMethod the HTTP method
-     */
-    public void setHttpMethod(final HttpMethod httpMethod) {
-        this.httpMethod = httpMethod;
-    }
-
-    /**
      * Check if http headers have been set for the REST request.
      *
      * @return true if headers have been set
      */
     public boolean checkHttpHeadersSet() {
         return httpHeaders != null && httpHeaders.length > 0;
-    }
-
-    /**
-     * Gets the http headers for the REST request.
-     *
-     * @return the headers
-     */
-    public String[][] getHttpHeaders() {
-        return httpHeaders;
     }
 
     /**
@@ -235,6 +200,11 @@ public class RestClientCarrierTechnologyParameters extends CarrierTechnologyPara
             result.setResult("url", ValidationStatus.INVALID,
                     "no proper URL has been set for event sending on REST client");
         }
+
+        if (StringUtils.isBlank(httpCodeFilter)) {
+            result.setResult(HTTP_CODE_FILTER, ValidationStatus.INVALID, "HTTP CODE FIlTER is blank.");
+        }
+
         return result;
     }
 
@@ -244,6 +214,8 @@ public class RestClientCarrierTechnologyParameters extends CarrierTechnologyPara
     @Override
     public String toString() {
         return "RestClientCarrierTechnologyParameters [url=" + url + ", httpMethod=" + httpMethod + ", httpHeaders="
-                        + Arrays.deepToString(httpHeaders) + "]";
+                        + Arrays.deepToString(httpHeaders) + ", httpCodeFilter=" + httpCodeFilter + "]";
     }
+
+
 }
