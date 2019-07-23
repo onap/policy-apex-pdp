07df95c4a945880d76dc200c2481b8b60b67997a
a.sreekumar <ajith.sreekumar@est.tech>
2019-07-23 16:38:32 +0000

    Extend APEX CLIEditor to generate policy in ToscaServiceTemplate format
    Currently apex CLIEditor is used to generate a json policy model from
    apex CLI language (.apex) file.
    As per the new  LifeCycle API, the policies are expected to be defined
    as ToscaServiceTemplate. Hence, the current CLIEditor is extended to
    generate the policies in ToscaServiceTemplate way.
    
    Change-Id: I2eb5d5b146643d40b623e329a2a63d6bb0c1fb42
    Issue-ID: POLICY-1885
    Signed-off-by: a.sreekumar <ajith.sreekumar@est.tech>

--- auth/cli-editor/src/main/java/org/onap/policy/apex/auth/clieditor/ApexCommandLineEditorMain.java
+++ auth/cli-editor/src/main/java/org/onap/policy/apex/auth/clieditor/ApexCommandLineEditorMain.java
@@ -1,19 +1,20 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2016-2018 Ericsson. All rights reserved.
+ *  Modifications Copyright (C) 2019 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
@@ -24,7 +25,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
-
+import org.onap.policy.apex.auth.clieditor.utils.CliUtils;
 import org.onap.policy.apex.model.utilities.json.JsonHandler;
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
@@ -64,7 +65,7 @@ public class ApexCommandLineEditorMain {
             parameters = parser.parse(args);
 
             if (parameters.isHelpSet()) {
-                parser.help(ApexCommandLineEditorMain.class.getName());
+                CliUtils.help(ApexCommandLineEditorMain.class.getName(), parser.getOptions());
                 return;
             }
             parameters.validate();
@@ -165,7 +166,7 @@ public class ApexCommandLineEditorMain {
 
     /**
      * Get the number of errors encountered in command processing.
-     * 
+     *
      * @return the number of errors
      */
     public int getErrorCount() {
@@ -174,7 +175,7 @@ public class ApexCommandLineEditorMain {
 
     /**
      * Sets the number of errors encountered in command processing.
-     * 
+     *
      * @param errorCount the number of errors
      */
     public void setErrorCount(final int errorCount) {
---
+++ auth/cli-editor/src/main/java/org/onap/policy/apex/auth/clieditor/tosca/ApexCliToscaEditorMain.java
@@ -0,0 +1,109 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2019 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.apex.auth.clieditor.tosca;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import lombok.Getter;
+import org.onap.policy.apex.auth.clieditor.ApexCommandLineEditorMain;
+import org.onap.policy.apex.auth.clieditor.CommandLineParameters;
+import org.onap.policy.apex.auth.clieditor.utils.CliUtils;
+import org.onap.policy.common.utils.coder.CoderException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class initiates an Apex CLI Tosca editor.
+ *
+ * @author Ajith Sreekumar (ajith.sreekumar@est.tech)
+ */
+public class ApexCliToscaEditorMain {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApexCliToscaEditorMain.class);
+
+    @Getter
+    private boolean failure;
+    private ApexCliToscaParameters parameters;
+    private ApexCommandLineEditorMain apexCliEditor;
+
+    /**
+     * Instantiates the Apex CLI Tosca editor.
+     *
+     * @param args the command line arguments
+     */
+    public ApexCliToscaEditorMain(final String[] args) {
+        final String argumentString = Arrays.toString(args);
+        LOGGER.info("Starting Apex CLI Tosca editor with arguments - {}", argumentString);
+
+        final ApexCliToscaParameterParser parser = new ApexCliToscaParameterParser();
+        parameters = parser.parse(args);
+        if (parameters.isHelpSet()) {
+            CliUtils.help(ApexCliToscaEditorMain.class.getName(), parser.getOptions());
+            return;
+        }
+        parameters.validate();
+
+        String policyModelFilePath = null;
+        try {
+            final File tempModelFile = File.createTempFile("policyModel",".json");
+            policyModelFilePath = tempModelFile.getAbsolutePath();
+        } catch (IOException e) {
+            LOGGER.error("Cannot create the policy model temp file.", e);
+        }
+
+        List<String> cliArgsList = CliUtils.generateArgumentsForCliEditor(parameters, parser.getOptionVariableMap(),
+            CommandLineParameters.class);
+        cliArgsList.add("-o");
+        cliArgsList.add(policyModelFilePath);
+        String[] cliArgs = cliArgsList.toArray(new String[cliArgsList.size()]);
+
+        apexCliEditor = new ApexCommandLineEditorMain(cliArgs);
+        if (apexCliEditor.getErrorCount() == 0) {
+            LOGGER.info("Apex CLI editor completed execution. Creating the ToscaPolicy using the tosca template"
+                + "skeleton file, config file, and policy model created.");
+
+            // Create the ToscaPolicy using the tosca template skeleton file, config file, and policy model created.
+            try {
+                CliUtils.createToscaServiceTemplate(parameters, policyModelFilePath);
+            } catch (IOException | CoderException e) {
+                failure = true;
+                LOGGER.error("Failed to create the ToscaPolicy using the generated policy model, apex config file and"
+                    + " the tosca template skeleton file.");
+            }
+        } else {
+            failure = true;
+            LOGGER.error("execution of Apex command line editor failed: {} command execution failure(s) occurred",
+                apexCliEditor.getErrorCount());
+        }
+
+    }
+
+    /**
+     * The main method, kicks off the cli tosca editor.
+     *
+     * @param args the arguments
+     */
+    public static void main(final String[] args) {
+        new ApexCliToscaEditorMain(args);
+    }
+}
