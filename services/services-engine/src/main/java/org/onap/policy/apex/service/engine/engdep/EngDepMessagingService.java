5610a707814ad39bfd5ea6fa176bafe44c97a213
a.sreekumar <ajith.sreekumar@est.tech>
2019-08-29 10:25:14 +0000

    Enable https support in ApexRestServer
    Enable https support in ApexRestServer by using the RestServer from policy-common
    
    Change-Id: I3dc8d88a499dfa9f0a82585a083526e236420c6e
    Issue-ID: POLICY-2016
    Signed-off-by: a.sreekumar <ajith.sreekumar@est.tech>

--- services/services-engine/src/main/java/org/onap/policy/apex/service/engine/engdep/EngDepMessagingService.java
+++ services/services-engine/src/main/java/org/onap/policy/apex/service/engine/engdep/EngDepMessagingService.java
@@ -5,15 +5,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
@@ -21,7 +21,6 @@
 package org.onap.policy.apex.service.engine.engdep;
 
 import java.net.InetSocketAddress;
-
 import org.onap.policy.apex.core.infrastructure.messaging.MessagingService;
 import org.onap.policy.apex.core.infrastructure.messaging.MessagingServiceFactory;
 import org.onap.policy.apex.core.infrastructure.messaging.util.MessagingUtils;
@@ -109,6 +108,6 @@ public class EngDepMessagingService {
     protected MessagingService<Message> getMessageService(final int port) {
         // Messaging service is used to transmit and receive messages over a communication protocol
         MessagingServiceFactory<Message> factory = new MessagingServiceFactory<>();
-        return factory.createServer(new InetSocketAddress(MessagingUtils.checkPort(port)));
+        return factory.createServer(new InetSocketAddress(MessagingUtils.findPort(port)));
     }
 }
