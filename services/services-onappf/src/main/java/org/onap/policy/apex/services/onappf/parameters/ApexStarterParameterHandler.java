e40b0baa925bcb6446b332e13544cc49451ec834
aditya.puthuparambil <aditya.puthuparambil@est.tech>
2020-03-04 14:31:19 +0000

    Fixing SONAR bug + code smell
    Change-Id: If76ddc860b67e0d8dcd4c48bacb5436ffc773faa
    Issue-ID: POLICY-1913
    Signed-off-by: aditya.puthuparambil <aditya.puthuparambil@est.tech>

--- services/services-onappf/src/main/java/org/onap/policy/apex/services/onappf/parameters/ApexStarterParameterHandler.java
+++ services/services-onappf/src/main/java/org/onap/policy/apex/services/onappf/parameters/ApexStarterParameterHandler.java
@@ -1,6 +1,6 @@
 /*-
  * ============LICENSE_START=======================================================
- *  Copyright (C) 2019 Nordix Foundation.
+ *  Copyright (C) 2019-2020 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,7 +60,6 @@ public class ApexStarterParameterHandler {
         } catch (final CoderException e) {
             final String errorMessage = "error reading parameters from \"" + arguments.getConfigurationFilePath()
                     + "\"\n" + "(" + e.getClass().getSimpleName() + "):" + e.getMessage();
-            LOGGER.error(errorMessage, e);
             throw new ApexStarterException(errorMessage, e);
         }
 
