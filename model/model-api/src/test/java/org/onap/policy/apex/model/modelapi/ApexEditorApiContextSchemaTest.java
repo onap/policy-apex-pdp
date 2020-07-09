e8717f09dd3099dd187c72e4d78e263bef5416f3
JvD_Ericsson <jeff.van.dam@est.tech>
2020-07-09 10:45:57 +0100

    Fix assertTrue SONAR issues in apex-pdp/model
    Replace assertTrue with assertEquals and assertFalse with
    assertNotEquals in apex-pdp/model
    
    Issue-ID: POLICY-2690
    Change-Id: Id37989ea8a6ad7b3156e13851cc072ce6c53c03e
    Signed-off-by: JvD_Ericsson <jeff.van.dam@est.tech>

--- model/model-api/src/test/java/org/onap/policy/apex/model/modelapi/ApexEditorApiContextSchemaTest.java
+++ model/model-api/src/test/java/org/onap/policy/apex/model/modelapi/ApexEditorApiContextSchemaTest.java
@@ -1,19 +1,20 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2016-2018 Ericsson. All rights reserved.
+ *  Modifications Copyright (C) 2020 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
@@ -21,7 +22,6 @@
 package org.onap.policy.apex.model.modelapi;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -41,45 +41,45 @@ public class ApexEditorApiContextSchemaTest {
         assertEquals(ApexApiResult.Result.FAILED, result.getResult());
 
         result = apexModel.loadFromFile("src/test/resources/models/PolicyModel.json");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
         result = apexModel.listContextSchemas(null, null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
 
         result = apexModel.createContextSchema("Hello", "0.0.2", "Java", "java.lang.String",
                 "1fa2e430-f2b2-11e6-bc64-92361f002671", "A description of hello");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
         result = apexModel.createContextSchema("Hello", "0.1.2", "Java", "java.lang.String",
                 "1fa2e430-f2b2-11e6-bc64-92361f002672", "A description of hola");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
         result = apexModel.createContextSchema("Hello", "0.1.4", "Java", "java.lang.String",
                 "1fa2e430-f2b2-11e6-bc64-92361f002672", "A description of connichi wa");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
         result = apexModel.createContextSchema("Hello", null, "Java", "java.lang.String", null, null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
         result = apexModel.createContextSchema("Hello", null, "Java", "java.lang.String", null, null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.CONCEPT_EXISTS));
+        assertEquals(ApexApiResult.Result.CONCEPT_EXISTS, result.getResult());
         result = apexModel.deleteContextSchema("Hello", "0.1.4");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
         result = apexModel.createContextSchema("Hello", "0.1.4", "Java", "java.lang.String",
                 "1fa2e430-f2b2-11e6-bc64-92361f002672", "A description of connichi wa");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
 
         result = apexModel.createContextSchema("Hello2", null, null, "java.lang.String", null, null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.FAILED));
+        assertEquals(ApexApiResult.Result.FAILED, result.getResult());
         result = apexModel.createContextSchema("Hello2", null, null, null, null, null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.FAILED));
+        assertEquals(ApexApiResult.Result.FAILED, result.getResult());
         result = apexModel.createContextSchema("Hello2", null, "Java", "java.lang.String", null, null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
 
         result = apexModel.createContextSchema("Hello", "0.1.2", "Java", "java.lang.Float",
                 "1fa2e430-f2b2-11e6-bc64-92361f002672", "A description of hola");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.CONCEPT_EXISTS));
+        assertEquals(ApexApiResult.Result.CONCEPT_EXISTS, result.getResult());
 
         result = apexModel.deleteContextSchema("Hello", "0.1.4");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
         result = apexModel.createContextSchema("Hello", "0.1.4", "Java", "java.lang.String",
                 "1fa2e430-f2b2-11e6-bc64-92361f002672", "A description of connichi wa");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
 
         result = apexModel.validateContextSchemas(null, null);
         assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
@@ -88,66 +88,66 @@ public class ApexEditorApiContextSchemaTest {
         assertEquals(ApexApiResult.Result.FAILED, result.getResult());
 
         result = apexModel.updateContextSchema("Hello", "0.0.2", null, null, null, "An updated description of hello");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
         result = apexModel.updateContextSchema("Hello", "0.0.2", null, null, null, null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
         result = apexModel.updateContextSchema("Hello", "0.1.2", "Java", "java.lang.Integer",
                 "1fa2e430-f2b2-11e6-bc64-92361f002673", "A further updated description of hola");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
 
         result = apexModel.updateContextSchema("Hello2", "0.0.2", null, null, null, "An updated description of hello");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.CONCEPT_DOES_NOT_EXIST));
+        assertEquals(ApexApiResult.Result.CONCEPT_DOES_NOT_EXIST, result.getResult());
 
         result = apexModel.listContextSchemas("@£%%$", null);
         assertEquals(ApexApiResult.Result.FAILED, result.getResult());
 
         result = apexModel.listContextSchemas("Hello", "0.1.2");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
-        assertTrue(result.getMessages().size() == 2);
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
+        assertEquals(2, result.getMessages().size());
 
         result = apexModel.listContextSchemas("Hello", null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
-        assertTrue(result.getMessages().size() == 4);
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
+        assertEquals(4, result.getMessages().size());
 
         result = apexModel.listContextSchemas(null, null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
-        assertTrue(result.getMessages().size() == 9);
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
+        assertEquals(9, result.getMessages().size());
 
         result = apexModel.deleteContextSchema("@£%%$", null);
         assertEquals(ApexApiResult.Result.FAILED, result.getResult());
 
         result = apexModel.deleteContextSchema("Hello", "0.1.1");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.CONCEPT_DOES_NOT_EXIST));
+        assertEquals(ApexApiResult.Result.CONCEPT_DOES_NOT_EXIST, result.getResult());
 
         result = apexModel.deleteContextSchema("Hellooooo", null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.CONCEPT_DOES_NOT_EXIST));
+        assertEquals(ApexApiResult.Result.CONCEPT_DOES_NOT_EXIST, result.getResult());
 
         result = apexModel.listContextSchemas("Hello", null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
-        assertTrue(result.getMessages().size() == 4);
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
+        assertEquals(4, result.getMessages().size());
 
         result = apexModel.deleteContextSchema("Hello", "0.1.2");
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
 
         result = apexModel.listContextSchemas("Hello", null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
-        assertTrue(result.getMessages().size() == 3);
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
+        assertEquals(3, result.getMessages().size());
 
         result = apexModel.deleteContextSchema("Hello", null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
 
         result = apexModel.listContextSchemas("Hello", null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.CONCEPT_DOES_NOT_EXIST));
+        assertEquals(ApexApiResult.Result.CONCEPT_DOES_NOT_EXIST, result.getResult());
 
         result = apexModel.listContextSchemas(null, null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
 
         result = apexModel.deleteContextSchema(null, null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
         assertEquals(5, result.getMessages().size());
 
         result = apexModel.listContextSchemas(null, null);
-        assertTrue(result.getResult().equals(ApexApiResult.Result.SUCCESS));
+        assertEquals(ApexApiResult.Result.SUCCESS, result.getResult());
         assertEquals(0, result.getMessages().size());
     }
 }
--- model/policy-model/src/test/java/org/onap/policy/apex/model/policymodel/concepts/TaskParameterTest.java
+++ model/policy-model/src/test/java/org/onap/policy/apex/model/policymodel/concepts/TaskParameterTest.java
@@ -1,20 +1,20 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2016-2018 Ericsson. All rights reserved.
- *  Modifications Copyright (C) 2019 Nordix Foundation.
+ *  Modifications Copyright (C) 2019-2020 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
@@ -22,10 +22,8 @@
 package org.onap.policy.apex.model.policymodel.concepts;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
@@ -36,7 +34,7 @@ import org.onap.policy.apex.model.basicmodel.concepts.AxValidationResult.Validat
 
 /**
  * Test task parameters.
- * 
+ *
  * @author Liam Fallon (liam.fallon@ericsson.com)
  */
 public class TaskParameterTest {
@@ -94,15 +92,15 @@ public class TaskParameterTest {
         assertEquals("AxTaskParameter:(key=AxReferenceKey:(parentKeyName=ParParentName,parentKeyVersion=0.0.1,"
                         + "parentLocalName=PLN,localName=LN),defaultValue=DefaultValue)", clonedPar.toString());
 
-        assertFalse(par.hashCode() == 0);
+        assertNotEquals(0, par.hashCode());
 
-        assertTrue(par.equals(par));
-        assertTrue(par.equals(clonedPar));
-        assertFalse(par.equals(null));
-        assertFalse(par.equals((Object) "Hello"));
-        assertFalse(par.equals(new AxTaskParameter(AxReferenceKey.getNullKey(), "DefaultValue")));
-        assertFalse(par.equals(new AxTaskParameter(parKey, "OtherDefaultValue")));
-        assertTrue(par.equals(new AxTaskParameter(parKey, "DefaultValue")));
+        assertEquals(par, par);
+        assertEquals(par, clonedPar);
+        assertNotNull(par);
+        assertNotEquals(par, (Object) "Hello");
+        assertNotEquals(par, new AxTaskParameter(AxReferenceKey.getNullKey(), "DefaultValue"));
+        assertNotEquals(par, new AxTaskParameter(parKey, "OtherDefaultValue"));
+        assertEquals(par, new AxTaskParameter(parKey, "DefaultValue"));
 
         assertEquals(0, par.compareTo(par));
         assertEquals(0, par.compareTo(clonedPar));
