3935b84cb5ca3d31d596ddfe6130b993cc2007ee
JvD_Ericsson <jeff.van.dam@est.tech>
2020-07-14 13:18:10 +0100

    Fix assertTrue in apex-pdp/model apex-pdp/plugins, apex-pdp/services and apex-pdp/testsuites
    Replace assertTrue with assertEquals and assertFalse with
    assertNotEquals in apex-pdp/model, apex-pdp/plugins, apex-pdp/services
    and apex-pdp/testsuites
    
    Issue-ID: POLICY-2690
    Change-Id: If088371cf012d5648e04ade2aa4d49b38945f6d2
    Signed-off-by: JvD_Ericsson <jeff.van.dam@est.tech>

--- model/policy-model/src/test/java/org/onap/policy/apex/model/policymodel/handling/PolicyModelSplitterTest.java
+++ model/policy-model/src/test/java/org/onap/policy/apex/model/policymodel/handling/PolicyModelSplitterTest.java
@@ -1,19 +1,20 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2016-2018 Ericsson. All rights reserved.
+ *  Modifications Copyright (C) 2020 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
@@ -22,7 +23,6 @@ package org.onap.policy.apex.model.policymodel.handling;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.Set;
@@ -53,7 +53,7 @@ public class PolicyModelSplitterTest {
         // not be in the split model
         apexModel.getEvents().getEventMap().remove(new AxArtifactKey("outEvent1", "0.0.1"));
         apexModel.getKeyInformation().getKeyInfoMap().remove(new AxArtifactKey("outEvent1", "0.0.1"));
-        assertTrue(apexModel.equals(splitApexModel));
+        assertEquals(splitApexModel, apexModel);
 
         final Set<AxArtifactKey> requiredMissingPolicySet = new TreeSet<AxArtifactKey>();
         requiredPolicySet.add(new AxArtifactKey("MissingPolicy", "0.0.1"));
@@ -77,7 +77,7 @@ public class PolicyModelSplitterTest {
         // not be in the split model
         apexModel.getEvents().getEventMap().remove(new AxArtifactKey("outEvent1", "0.0.1"));
         apexModel.getKeyInformation().getKeyInfoMap().remove(new AxArtifactKey("outEvent1", "0.0.1"));
-        assertTrue(apexModel.equals(splitApexModel));
+        assertEquals(splitApexModel, apexModel);
 
         // There's only one policy so a split of this model on that policy should return the same
         // model
--- testsuites/integration/integration-executor-test/src/test/java/org/onap/policy/apex/testsuites/integration/executor/handling/TestApexModelExport.java
+++ testsuites/integration/integration-executor-test/src/test/java/org/onap/policy/apex/testsuites/integration/executor/handling/TestApexModelExport.java
@@ -1,28 +1,29 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2016-2018 Ericsson. All rights reserved.
+ *  Modifications Copyright (C) 2020 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
 
 package org.onap.policy.apex.testsuites.integration.executor.handling;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -88,13 +89,13 @@ public class TestApexModelExport {
         model.getKeyInformation().getKeyInfoMap().remove(new AxArtifactKey("TestContextItem00B", "0.0.1"));
         model.getKeyInformation().getKeyInfoMap().remove(new AxArtifactKey("TestContextItem00C", "0.0.1"));
 
-        assertTrue(model.equals(exportedModel0));
+        assertEquals(exportedModel0, model);
 
         exportPolicyList.remove(0);
 
         final AxPolicyModel exportedModel1 = PolicyModelSplitter.getSubPolicyModel(model, exportPolicyList);
-        assertFalse(model.equals(exportedModel1));
-        assertTrue(model.getPolicies().get("Policy1").equals(exportedModel1.getPolicies().get("Policy1")));
+        assertNotEquals(exportedModel1, model);
+        assertEquals(exportedModel1.getPolicies().get("Policy1"), model.getPolicies().get("Policy1"));
 
         exportPolicyList.clear();
         exportPolicyList.add(new AxArtifactKey("NonExistentPolicy", "0.0.1"));
@@ -103,11 +104,10 @@ public class TestApexModelExport {
             final AxPolicyModel emptyExportedModel = PolicyModelSplitter.getSubPolicyModel(model, exportPolicyList);
             assertNotNull(emptyExportedModel);
         } catch (final Exception e) {
-            assertTrue(e.getMessage()
-                    .equals("new model is invalid:\n" + "***validation of model failed***\n"
+            assertEquals("new model is invalid:\n" + "***validation of model failed***\n"
                             + "AxPolicies:Policies:0.0.1 - policyMap may not be null or empty\n"
                             + "AxEvents:Events:0.0.1 - eventMap may not be null or empty\n"
-                            + "********************************"));
+                            + "********************************", e.getMessage());
         }
     }
 }
