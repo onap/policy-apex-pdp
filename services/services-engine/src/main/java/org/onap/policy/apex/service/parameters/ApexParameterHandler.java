e3b62e2aa2513c206790fab7e3fb10147ff70340
a.sreekumar <ajith.sreekumar@bell.ca>
2021-01-25 11:03:42 +0000

    Improve handling of multiple policy in APEX PDP
    Change-Id: Ic4adf5bd8876dc31fc93993298e90389baaa2c39
    Issue-ID: POLICY-2883
    Signed-off-by: a.sreekumar <ajith.sreekumar@bell.ca>

--- services/services-engine/src/main/java/org/onap/policy/apex/service/parameters/ApexParameterHandler.java
+++ services/services-engine/src/main/java/org/onap/policy/apex/service/parameters/ApexParameterHandler.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2016-2018 Ericsson. All rights reserved.
  *  Modifications Copyright (C) 2019 Nordix Foundation.
- *  Modifications Copyright (C) 2020 Bell Canada. All rights reserved.
+ *  Modifications Copyright (C) 2020-2021 Bell Canada. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -72,8 +72,6 @@ public class ApexParameterHandler {
      * @throws ParameterException on parameter exceptions
      */
     public ApexParameters getParameters(final ApexCommandLineArguments arguments) throws ParameterException {
-
-        ParameterService.clear();
 
         ApexParameters parameters = null;
         String toscaPolicyFilePath = arguments.getToscaPolicyFilePath();
