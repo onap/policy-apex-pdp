4ccddb1db434b1e01b158c4501a7d960d579f5c9
liamfallon <liam.fallon@est.tech>
2021-02-10 14:01:23 +0000

    Remove URL and useerid from JMS plugin
    This review makes the default URL and userid for JMS null, the actual
    values must be specified as parameters.
    
    Issue-ID: POLICY-3035
    Change-Id: I9c06d94d8816092425e2df58b849d837ad953fb2
    Signed-off-by: liamfallon <liam.fallon@est.tech>

--- core/core-engine/src/main/java/org/onap/policy/apex/core/engine/executor/context/StateFinalizerExecutionContext.java
+++ core/core-engine/src/main/java/org/onap/policy/apex/core/engine/executor/context/StateFinalizerExecutionContext.java
@@ -28,7 +28,6 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.TreeMap;
 import lombok.Getter;
-import lombok.Setter;
 import org.onap.policy.apex.context.ContextAlbum;
 import org.onap.policy.apex.context.ContextRuntimeException;
 import org.onap.policy.apex.core.engine.context.ApexInternalContext;
