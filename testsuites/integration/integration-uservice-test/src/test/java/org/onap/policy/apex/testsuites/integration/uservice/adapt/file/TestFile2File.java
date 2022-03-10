224d4e486871c9d16f1dd3289361684b76efb064
liamfallon <liam.fallon@est.tech>
2022-03-10 20:59:57 +0000

    Remove bad dependency in apex POM
    XML event protocol was removed as jax-rs dependencies were removed in
    Jakarta.
    
    Issue-ID: POLICY-3982
    Change-Id: I5e8881ae8062fd46b46a1ec655e1210b8ccf2fbf
    Signed-off-by: liamfallon <liam.fallon@est.tech>

--- testsuites/integration/integration-uservice-test/src/test/java/org/onap/policy/apex/testsuites/integration/uservice/adapt/file/TestFile2File.java
+++ testsuites/integration/integration-uservice-test/src/test/java/org/onap/policy/apex/testsuites/integration/uservice/adapt/file/TestFile2File.java
@@ -1,7 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2016-2018 Ericsson. All rights reserved.
- *  Modifications Copyright (C) 2020-2021 Nordix Foundation.
+ *  Modifications Copyright (C) 2020-2022 Nordix Foundation.
  *  Modifications Copyright (C) 2020-2022 Bell Canada. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -48,13 +48,6 @@ public class TestFile2File {
         final String[] args = {"-rfr", "target", "-p", "target/examples/config/SampleDomain/File2FileJsonEvent.json"};
 
         testFileEvents(args, "target/examples/events/SampleDomain/EventsOut.json", 44400);
-    }
-
-    @Test
-    public void testXmlFileEvents() throws MessagingException, ApexException, IOException {
-        final String[] args = {"-rfr", "target", "-p", "target/examples/config/SampleDomain/File2FileXmlEvent.json"};
-
-        testFileEvents(args, "target/examples/events/SampleDomain/EventsOut.xmlfile", 100000);
     }
 
     private void testFileEvents(final String[] args, final String outFilePath, final long expectedFileSize)
