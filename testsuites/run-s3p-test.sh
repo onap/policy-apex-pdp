a5750bcdce833cd4a06ec4ec9bcd4ae395004143
waynedunican <wayne.dunican@est.tech>
2023-01-10 11:06:36 +0000

    Automate S3P tests in Apex
    - Trigger S3P test execution
    
    Issue-ID: POLICY-4156
    Change-Id: Iaf73a3b39b76c0239cc518fb87a6cf02b41d2e26
    Signed-off-by: Wayne Dunican <wayne.dunican@est.tech>

---
+++ testsuites/run-s3p-test.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# ============LICENSE_START=======================================================
+#  Copyright (C) 2023 Nordix Foundation. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# ============LICENSE_END=========================================================
+
+#===MAIN===#
+if [ -z "${WORKSPACE}" ]; then
+    export WORKSPACE=$(git rev-parse --show-toplevel)
+fi
+
+export TESTDIR=${WORKSPACE}/testsuites
+export APEX_PERF_TEST_FILE=$TESTDIR/performance/performance-benchmark-test/src/main/resources/apexPdpPerformanceTestPlan.jmx
+export APEX_STAB_TEST_FILE=$TESTDIR/apex-pdp-stability/src/main/resources/apexPdpStabilityTestPlan.jmx
+
+if [ $1 == "run" ]
+then
+
+  mkdir automate-performance;cd automate-performance;
+  git clone "https://gerrit.onap.org/r/policy/docker"
+  cd docker/csit
+
+  if [ $2 == "performance" ]
+  then
+    bash start-s3p-tests.sh run $APEX_PERF_TEST_FILE;
+  elif [ $2 == "stability" ]
+  then
+    bash start-s3p-tests.sh run $APEX_STAB_TEST_FILE;
+  else
+    echo "echo Invalid arguments provided. Usage: $0 [option..] {performance | stability}"
+  fi
+
+else
+  echo "Invalid arguments provided. Usage: $0 [option..] {run | uninstall}"
+fi
+
