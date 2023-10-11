4dfbf8f9fd6b3a0afc46b313028c05914328cfca
saul.gill <saul.gill@est.tech>
2023-10-11 13:54:22 +0100

    Fix run-s3p-test.sh
    Add missing arg in script
    
    Issue-ID: POLICY-4853
    Change-Id: I13ebc1351b32877813d266f2dbdf86dfa7b05e04
    Signed-off-by: saul.gill <saul.gill@est.tech>

--- testsuites/run-s3p-test.sh
+++ testsuites/run-s3p-test.sh
@@ -35,10 +35,10 @@ then
 
   if [ $2 == "performance" ]
   then
-    bash start-s3p-tests.sh run $APEX_PERF_TEST_FILE;
+    bash start-s3p-tests.sh run $APEX_PERF_TEST_FILE apex-pdp;
   elif [ $2 == "stability" ]
   then
-    bash start-s3p-tests.sh run $APEX_STAB_TEST_FILE;
+    bash start-s3p-tests.sh run $APEX_STAB_TEST_FILE apex-pdp;
   else
     echo "echo Invalid arguments provided. Usage: $0 [option..] {performance | stability}"
   fi
