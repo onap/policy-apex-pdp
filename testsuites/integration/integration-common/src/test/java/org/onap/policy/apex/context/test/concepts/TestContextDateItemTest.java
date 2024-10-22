bc949bab9233b43224d9d9fab5243f7b62b7751e
akenihan <adam.kenihan@est.tech>
2024-10-22 17:49:23 +0100

    Improve coverage on testsuites module in apex-pdp
    Issue-ID: POLICY-5059
    Change-Id: I56ce8c31c9c291b11e1036bcf1abec71eb4b8aeb
    Signed-off-by: akenihan <adam.kenihan@est.tech>

---
+++ testsuites/integration/integration-common/src/test/java/org/onap/policy/apex/context/test/concepts/TestContextDateItemTest.java
@@ -0,0 +1,39 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2024 Nordix Foundation. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.apex.context.test.concepts;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.Date;
+import org.junit.jupiter.api.Test;
+
+class TestContextDateItemTest {
+
+    @Test
+    void contextDateItemDateTest() {
+        TestContextDateItem tcdi = new TestContextDateItem();
+        assertNotEquals(tcdi.getDateValue(), new Date(2323223232L));
+        assertNotEquals(new TestContextDateItem(null), new TestContextDateItem(new Date(2323223232L)));
+        tcdi.setDateValue(new Date(32323232223L));
+        assertNotEquals(new TestContextDateItem(2323223232L), tcdi);
+        tcdi.setDateValue(null);
+    }
+}
