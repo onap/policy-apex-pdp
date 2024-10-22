5777741547a2be73d2bdcadfe164d634ab909dae
akenihan <adam.kenihan@est.tech>
2024-10-22 17:49:23 +0100

    Improve coverage on testsuites module in apex-pdp
    Issue-ID: POLICY-5059
    Change-Id: I56ce8c31c9c291b11e1036bcf1abec71eb4b8aeb
    Signed-off-by: akenihan <adam.kenihan@est.tech>

---
+++ testsuites/integration/integration-common/src/test/java/org/onap/policy/apex/context/test/concepts/TestContextDateLocaleItemTest.java
@@ -0,0 +1,44 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2018 Ericsson. All rights reserved.
+ *  Modifications Copyright (C) 2024 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.apex.context.test.concepts;
+
+import java.util.TimeZone;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class TestContextDateLocaleItemTest {
+
+    @Test
+    void contextDateLocaleItemTest() {
+        TestContextDateLocaleItem tcdli = new TestContextDateLocaleItem();
+        TestContextDateItem tcdi = new TestContextDateItem();
+        String tzValue = TimeZone.getTimeZone("Europe/London").getDisplayName();
+        Assertions.assertNotEquals(new TestContextDateLocaleItem(tcdi, tzValue, false, 0, "English", "England"),
+                new TestContextDateLocaleItem());
+        Assertions.assertNotEquals(tcdli, new TestContextDateLocaleItem(tcdli));
+        tcdli.setTzValue(TimeZone.getTimeZone("Europe/Dublin").getDisplayName());
+        TestContextDateLocaleItem nullTcdli = new TestContextDateLocaleItem();
+        nullTcdli.setTzValue(null);
+        Assertions.assertNotEquals(tcdli, nullTcdli);
+    }
+}
---
+++ testsuites/integration/integration-common/src/test/java/org/onap/policy/apex/context/test/concepts/TestContextDateTzItemTest.java
@@ -0,0 +1,38 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2018 Ericsson. All rights reserved.
+ *  Modifications Copyright (C) 2024 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.apex.context.test.concepts;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TestContextDateTzItemTest {
+
+    @Test
+    void contextDateTzItemTest() {
+        TestContextDateTzItem tcdti = new TestContextDateTzItem();
+        TestContextDateTzItem tcdti1 = new TestContextDateTzItem();
+        tcdti1.setTzValue("Europe/London");
+        tcdti.setTzValue(null);
+        Assertions.assertNotEquals(tcdti, tcdti1);
+        Assertions.assertEquals(tcdti, new TestContextDateTzItem(tcdti));
+    }
+}
