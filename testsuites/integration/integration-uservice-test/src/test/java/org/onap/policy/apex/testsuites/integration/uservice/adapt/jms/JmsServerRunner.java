5deb7e9591a7a8bea9f7d743c3abaad7d39de450
danielhanrahan <daniel.hanrahan@est.tech>
2026-06-11 13:38:42 +0100

    Add zookeeper version for CVE fixes
    - Override zookeeper version to 3.9.5
    - Minor test fix to prevent test failure if disk usage is high
    
    Issue-ID: POLICY-5592
    Change-Id: I8d519ad532d51c3e27d8e3e7ea61ac876e6130b2
    Signed-off-by: danielhanrahan <daniel.hanrahan@est.tech>

--- testsuites/integration/integration-uservice-test/src/test/java/org/onap/policy/apex/testsuites/integration/uservice/adapt/jms/JmsServerRunner.java
+++ testsuites/integration/integration-uservice-test/src/test/java/org/onap/policy/apex/testsuites/integration/uservice/adapt/jms/JmsServerRunner.java
@@ -1,6 +1,6 @@
 /*-
  * ============LICENSE_START=======================================================
- *  Copyright (C) 2022-2024 Nordix Foundation.
+ *  Copyright (C) 2022-2026 OpenInfra Foundation Europe. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,6 +56,7 @@ public class JmsServerRunner implements Runnable {
 
         config.addAcceptorConfiguration(serverName, serverUri);
         config.setSecurityEnabled(false);
+        config.setMaxDiskUsage(100);
         config.setJournalDirectory("target/artemisActiveMq/data/journal");
         config.setBindingsDirectory("target/artemisActiveMq/data/bindings");
         config.setLargeMessagesDirectory("target/artemisActiveMq/data/largemessages");
